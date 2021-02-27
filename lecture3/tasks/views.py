3b924fc7dd83155ceff6a816cd3f2231427b48a2
timothyryeyn <timothyryeyn.perez@gmail.com>
2021-02-27 16:29:07 +0800

    Add redirect to list after adding of task

--- lecture3/tasks/views.py
+++ lecture3/tasks/views.py
@@ -1,7 +1,9 @@
 from django import forms
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
-tasks = ["foo", "bar", "baz"]
+tasks = []
 
 class NewTaskForm(forms.Form):
     task = forms.CharField(label="New Task")
@@ -19,6 +21,7 @@ def add(request):
         if form.is_valid():
             task = form.cleaned_data["task"]
             tasks.append(task)
+            return HttpResponseRedirect(reverse("tasks:index"))
         else:
             return render(request, "tasks/add.html", {
                 "form": form
